8f03b9c00ce4820384a8a1eb6dd14d69cb09efd3
AbhiBirla <er.abhi.birla@gmail.com>
2021-03-15 20:35:48 +0530

    package

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-      <BugReport user = "ronit" app_id = "test" color = "brown" reportLink = "" />
+      <BugReport user = "bondsindia" app_id = "test" color = "brown" reportLink = "" />
       <header className="App-header">
         <img style = {{marginLeft : '50vw'}} src={logo} className="App-logo" alt="logo" />
         <p>
