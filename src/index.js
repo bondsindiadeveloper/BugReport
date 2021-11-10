caad30385b199909d0d10fafcbb45d788593bb28
khatrironit <ronitkhatri1999@gmail.com>
2021-11-10 10:31:26 +0530

    bug icon fixed

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-      <BugReport user = "bondsindia" app_id = "test" reportLink = "" displayText={`Report \n a Bug`} backgroundstyle={{
-    color : "#585858",fontSize : "15px" ,fontWeight:"bolder",lineHeight:1.2, verticalAlign:"middle",textAlign:"center",padding:"2px",whiteSpace: "pre-line"
-  }} imgstyle={{color : "#585858", width : "30px" , height : "30px" ,verticalAlign:"middle"}} headerAuthParam="timepass" />
+        <div style={{position: 'fixed'}}>
+          <BugReport user = "bondsindia" app_id = "test" reportLink = "" displayText={`Report \n a Bug`} backgroundstyle={{
+            color : "#585858",fontSize : "15px" ,fontWeight:"bolder",lineHeight:1.2, verticalAlign:"middle",textAlign:"center",padding:"2px",whiteSpace: "pre-line"
+          }} imgstyle={{color : "#585858", width : "30px" , height : "30px" ,verticalAlign:"middle"}} headerAuthParam="timepass" />
+        </div>
+      
       <header className="App-header">
         <img style = {{marginLeft : '50vw'}} src={logo} className="App-logo" alt="logo" />
         <p>
