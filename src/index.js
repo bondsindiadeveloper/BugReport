c8b6c61af5bae7a785a3dc1c8a09063d5695844f
khatrironit <ronitkhatri1999@gmail.com>
2022-05-31 13:37:44 +0530

    server side updates

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React ,{Component}from 'react';
 import { render } from "react-dom";
 import BugReport from './lib/index'
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 class App extends Component {
   render(){
--- src/lib/components/BugReport.js
+++ src/lib/components/BugReport.js
@@ -3,7 +3,6 @@ import BugIcon from "mdi-react/BugIcon";
 import html2canvas from "html2canvas";
 import Modals from "./Modal";
 import axios from 'axios'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Spinner from './Spinner';
 import Bugimgicon from "./bugicon.svg" 
 class BugReport extends Component {
